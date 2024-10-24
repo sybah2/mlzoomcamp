981d942c46016475f3e7d174b1c163fec1b33efe
sybah2 <saikouybah@gmail.com>
2024-10-24 14:15:55 +0100

    Updated question 4

--- week5/question4_predict.py
+++ week5/question4_predict.py
@@ -1,22 +1,23 @@
 import pickle
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask import request
+from flask import jsonify
 
 
-dv = 'dv.bin'
-model = 'model1.bin'
 
-with open(dv, 'rb') as f_in:
-    dv = pickle.load(f_in)
+def load(filename: str):
+    with open(filename, 'rb') as f_in:
+        return pickle.load(f_in)
 
-with open(model, 'rb') as f_in:
-    model = pickle.load(f_in)
+
+dv = load('./dv.bin')
+model = load('./model1.bin')
 
 app = Flask('churn')
 
-@app.route('/churn', methods=['POST'])
 
+@app.route('/churn', methods=['POST'])
 def predict():
-    # json = Python dictionary
     customer = request.get_json()
  
     X = dv.transform([customer])
@@ -24,7 +25,6 @@ def predict():
     churn = y_pred >= 0.5
  
     result = {
- 
         'churn_probability': float(y_pred),
         'churn': bool(churn)
     }
@@ -32,6 +32,5 @@ def predict():
     return jsonify(result) 
 
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=9696)
+    app.run(debug=True, host="0.0.0.0", port=9696)
--- week5/question4_test.py
+++ week5/question4_test.py
@@ -1,8 +1,7 @@
 import requests
 
-url = 'http://127.0.0.1:9696/predict'
+url = 'http://localhost:9696/predict'
 
-customer = 'abz_123'
 
 client = {"job": "student", "duration": 280, "poutcome": "failure"}
 score = requests.post(url, json=client).json()
